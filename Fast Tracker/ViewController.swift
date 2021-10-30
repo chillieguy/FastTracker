1bea3cbb84c0832a83c3e48e5fde38aeab17cb3f
Chuck Underwood <chillieguy@live.com>
2021-10-30 08:15:49 -0700

    Initial Commit

---
+++ Fast Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fast Tracker
+//
+//  Created by Chuck Underwood on 10/30/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Fast TrackerUITests/Fast_TrackerUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Fast_TrackerUITestsLaunchTests.swift
+//  Fast TrackerUITests
+//
+//  Created by Chuck Underwood on 10/30/21.
+//
+
+import XCTest
+
+class Fast_TrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
