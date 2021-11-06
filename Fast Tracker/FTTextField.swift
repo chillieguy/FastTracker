0a057160b238dd5f6c85f1498cdfe48980bdc5f2
Chuck Underwood <chillieguy@live.com>
2021-11-06 15:53:25 -0700

    Setup custom Textfield

---
+++ Fast Tracker/FTTextField.swift
@@ -0,0 +1,38 @@
+//
+//  FTTextField.swift
+//  Fast Tracker
+//
+//  Created by Chuck Underwood on 11/6/21.
+//
+
+import UIKit
+
+class FTTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.systemGray4.cgColor
+        
+        textColor = .label
+        tintColor = .label
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        
+    }
+}
