c86e0b4176fec52218987640b4a712bb48cabb26
Chuck Underwood <chillieguy@live.com>
2021-11-07 07:35:05 -0800

    Add title label component

---
+++ Fast Tracker/Custom Components/FTTitleLabel.swift
@@ -0,0 +1,40 @@
+//
+//  FTTitleLabel.swift
+//  Fast Tracker
+//
+//  Created by Chuck Underwood on 11/7/21.
+//
+
+import UIKit
+
+class FTTitleLabel: UILabel {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingTail
+    
+    }
+
+}
